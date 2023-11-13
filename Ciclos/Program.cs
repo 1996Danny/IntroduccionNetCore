e52d0ed4363794c9b9f21426a97f9a827842d4fd
Frías Néstor Daniel <friasdaniel996@gmail.com>
2023-11-13 16:37:46 -0300

    05

---
+++ Ciclos/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //(declaramos al variable i y la igualamos a 0; condicion i menor a 10; cada vez que cumpla un ciclo se incrementa i en 1)
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Resultado {0}",i);
+            }
+
+            //decremento
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine("Resultado {0}", i);
+            }
+
+            //creamos un array string
+            string[] arr = { "Dany", "Jose", "Pepito" };
+            int[] anio = { 24, 37, 40 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Hola, {0}", arr[i] + " de " + anio[i] + " años ");
+            }
+            // mas simplificado para obtener los elemtos de una coleccion de datos...
+            foreach (var item in arr)
+            {
+                Console.WriteLine("Hola, {0}", item);
+            }
+        }
+    }
+}
---
+++ TrianguloApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TrianguloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pisos = 0;
+
+            // Mensaje para el ingreso de datos
+            Console.WriteLine("Ingrese el número de pisos: ");
+
+            // Ingreso de datos y conversión de str a int
+            pisos = Convert.ToInt16(Console.ReadLine());
+
+            int[] arreglo = new int[1];
+
+            for (int i = 0; i < pisos; i++)
+            {
+                int[] pascal = new int[i + 1];
+
+                // Espacios en blanco antes de los números
+                for (int j = 0; j < pisos - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    if (k == 0 || k == i)
+                    {
+                        pascal[k] = 1;
+                    }
+                    else
+                    {
+                        pascal[k] = arreglo[k] + arreglo[k - 1];
+                    }
+
+                    Console.Write("[" + pascal[k] + "]");
+                }
+
+                arreglo = pascal;
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
